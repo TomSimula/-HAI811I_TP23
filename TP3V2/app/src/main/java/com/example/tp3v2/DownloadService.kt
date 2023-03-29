d90ae40ba946a60cdd887f5d50be8b388d68de38
Tom Simula <tom.simul@gmail.com>
2023-03-29 11:57:00 +0200

    Fix download

--- TP3V2/app/src/main/java/com/example/tp3v2/DownloadService.kt
+++ TP3V2/app/src/main/java/com/example/tp3v2/DownloadService.kt
@@ -4,8 +4,11 @@ import android.app.IntentService
 import android.content.Context
 import android.content.Intent
 import android.util.Log
-import androidx.core.app.ActivityCompat.requestPermissions
-import androidx.core.content.ContentProviderCompat.requireContext
+import android.app.DownloadManager
+import android.os.Environment
+import androidx.core.net.toUri
+import android.net.ConnectivityManager
+import android.content.BroadcastReceiver
 
 class DownloadService : IntentService("DownloadService"){
 
@@ -15,24 +18,22 @@ class DownloadService : IntentService("DownloadService"){
 
 
         var connectionManager =
-            getSystemService(Context.CONNECTIVITY_SERVICE) as android.net.ConnectivityManager
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         var networkInfo = connectionManager.activeNetwork
         if (networkInfo != null) {
             var url = "https://lafertemace.fr/liste-jeux.pdf"
             var downloadManager =
-                getSystemService(Context.DOWNLOAD_SERVICE) as android.app.DownloadManager
-            var request = android.app.DownloadManager.Request(android.net.Uri.parse(url))
-            request.setAllowedNetworkTypes(android.app.DownloadManager.Request.NETWORK_WIFI or android.app.DownloadManager.Request.NETWORK_MOBILE)
-            request.setTitle("Download")
-            request.setDescription("Downloading file...")
-            request.setNotificationVisibility(android.app.DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-            request.setDestinationInExternalPublicDir(
-                android.os.Environment.DIRECTORY_DOWNLOADS,
-                "${System.currentTimeMillis()}"
-            )
+                getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+            var request = DownloadManager.Request(url.toUri())
+                .setMimeType("application/pdf")
+                .setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
+                .setTitle("liste-jeux.pdf")
+                .setDescription("Downloading file...")
+                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "liste-jeux.pdf")
             downloadManager.enqueue(request)
             //notify user when download is completed
-            var receiver = object : android.content.BroadcastReceiver() {
+            var receiver = object : BroadcastReceiver() {
                 override fun onReceive(context: Context?, intent: Intent?) {
                     android.widget.Toast.makeText(
                         context,
