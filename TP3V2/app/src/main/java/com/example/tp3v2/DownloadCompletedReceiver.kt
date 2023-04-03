380d6c3e688e39b9b019f7d7970044a187288b53
Daniel Azevedo Gomes <danimbc@gmail.com>
2023-04-03 19:12:02 +0200

    info pdf

--- TP3V2/app/src/main/java/com/example/tp3v2/DownloadCompletedReceiver.kt
+++ TP3V2/app/src/main/java/com/example/tp3v2/DownloadCompletedReceiver.kt
@@ -3,9 +3,15 @@ package com.example.tp3v2
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.graphics.pdf.PdfRenderer
 import android.os.Environment
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import org.w3c.dom.Text
 import java.io.File
+
 
 class DownloadCompletedReceiver: BroadcastReceiver() {
 
@@ -16,16 +22,36 @@ class DownloadCompletedReceiver: BroadcastReceiver() {
 
             val filep = File(path)
 
-            Log.d("file path", "${filep.absoluteFile}")
+
+            if (filep.exists()) {
+                lateinit var version: String
+                lateinit var author: String
+                lateinit var title: String
+                lateinit var creation_date: String
+                lateinit var producer: String
 
 
-            if (filep != null) {
-                if (filep.exists()) {
-                    filep.readText()
-                    Log.d("DownloadService", "${filep.readText()}")
-                } else {
-                    Log.d("DownloadService", "File does not exist")
+                val content = filep.readText()
+                for(line in content.lines()){
+                    if(line.startsWith("%PDF")) version = line.split("-")[1]
+                    if(line.startsWith("/Author")) author = line.split(" ")[1].replace("(", "").replace(")", "")
+                    if(line.startsWith("/Title")) title = line.substring(8, line.length - 1)
+                    if(line.startsWith("/CreationDate")) creation_date = line.split(" ")[1].replace("(", "").replace(")", "")
+                    if(line.startsWith("/Producer")) producer = line.substring(11, line.length - 1)
                 }
+
+                Log.d("version", version)
+                Log.d("author", author)
+                Log.d("title", title)
+                Log.d("creation_date", creation_date)
+                Log.d("producer", producer)
+
+                val info = "Version: $version\nAuthor: $author\nTitle: $title\nCreation date: $creation_date\nProducer: $producer"
+                var view = View.inflate(context, R.layout.fragment_inscription, null)
+                view.findViewById<TextView>(R.id.inscrFragTextViewPDFInformation).text = info
+
+            } else {
+                Log.d("DownloadService", "File does not exist")
             }
         }
     }
